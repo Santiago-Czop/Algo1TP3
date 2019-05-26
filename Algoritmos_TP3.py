b24e8b71f34b99cc683dc99fe291ff2bcc741056
marian1908 <51036899+marian1908@users.noreply.github.com>
2019-05-26 20:56:02 -0300

    Add files via upload

--- Algoritmos_TP3.py
+++ Algoritmos_TP3.py
@@ -1,3 +1,11 @@
+from sys import argv
+from csv import reader
+
+PROGRAMA = 0
+ARCHIVO_SL = 1
+ITERACIONES = 2
+ARCHIVO_SVG = 3
+
 class ListaEnlazada:
     def __init__(self):
         self.prim = None #AXIOMA
@@ -61,30 +69,56 @@ class Trazo:
         self.vector_inicial
 
 def main():
-    procesar_y_leer_archivos()
-    
-    func()
-
+    try:
+        validar_parametros()
+        angulo, axioma, reglas = cargar_archivo(argv[ARCHIVO_SL])
+    except Exception as e:
+        print(e)
+        return
+    resultado = obtener_resultado(axioma, reglas, int(argv[ITERACIONES]))
     escribir_svg()
 
-def procesar_y_leer_archivos():
-    leer_sl()
-    crear_diccionario()
-    
-    return diccionario_de_reglas, axioma, angulo, alfabeto
+def validar_parametros():
+    if len(argv) != 4:
+        raise IndexError(f"El programa {argv[PROGRAMA]} debe recibir 3 parámetros.")
+    elif argv[ARCHIVO_SL][-3:].lower() != ".sl":
+        raise Exception("El primer parámetro debe ser un archivo .sl")
+    elif not argv[ITERACIONES].isdigit():
+        raise TypeError("El segundo parámetro debe ser un número entero mayor o igual a cero.")
+    elif argv[ARCHIVO_SVG][-4:].lower() != ".svg":
+        raise Exception("El tercer parámetro debe ser un archivo .svg")
 
-def func(anterior, lista, d, n):
-    #Llevar la cuenta
-    if n == 0:
-        return
+def cargar_archivo(ruta_archivo):
+    reglas = {}
+    try:
+        with open(ruta_archivo) as archivo:
+            lector = reader(archivo, delimiter = " ")
+            try:
+                angulo = float(archivo.readline().rstrip())
+            except ValueError:
+                raise ValueError("El valor del ángulo no es válido.")
+            axioma = archivo.readline().rstrip()
+            try:
+                for predecesor, sucesor in lector:
+                    reglas[predecesor] = sucesor
+            except:
+                raise Exception("El formato de las reglas no es válido.")
+            return angulo, axioma, reglas
+    except FileNotFoundError:
+        raise FileNotFoundError(f"El archivo {ruta_archivo} no existe.")
+    except IOError:
+        raise IOError(f"El archivo {ruta_archivo} no se encuentra disponible.")
 
-
-    for nodo in listaenlazada:
-        lista.reemplazar(anterior, nodo, d[nodo.dato])
-        n -= 1
-        func(anterior, lista, d, n)
-
-        anterior = nodo
+def obtener_resultado(cadena, reglas, cantidad):
+    if cantidad == 0:
+        return cadena
+    else:
+        resultado = ""
+        for c in cadena:
+            resultado += reglas.get(c, c)
+        cantidad -= 1
+        resultado = obtener_resultado(resultado, reglas, cantidad)
+        return resultado
 
 def recorrer_lista(lista)
     xmin = 0
@@ -124,3 +158,5 @@ def calcular_posicion_nueva()
     posicion_original = posicion_tortuga
     posicion_nueva = posicion_original + (x= sin alfa * norma_tortuga, y= cos alfa * norma tortuga)
     return posicion_nueva
+
+main()
